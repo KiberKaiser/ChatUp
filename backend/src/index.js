f00bc80a92b02238d1c17b4d3e6829a350731452
CyberKaiser <48377917+KiberKaiser@users.noreply.github.com>
2026-04-07 01:57:09 +0300

    Set default port and client URL in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,14 +13,17 @@ import { app, server } from './lib/socket.js'
 
 dotenv.config()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5001
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173"
 
 app.use(express.json({ limit: "20mb" }))
 app.use(express.urlencoded({ extended: true, limit: "20mb" }))
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     credentials: true
 }))
 
